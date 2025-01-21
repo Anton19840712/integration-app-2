f19891e8befd84b3559481c65290fc159f3ea4a7
Gridushko Anton Anatolievich <gridjushko@pit.protei.ru>
2025-01-21 10:35:45 +0300

    Удали тестовые проекты.

--- tcp-server/Program.cs
+++ tcp-server/Program.cs
@@ -1,50 +1,38 @@
-using System.Net.Sockets;
-using System.Net;
-using System.Text;
+using Serilog;
 
-Console.Title = "TCP Server";
-Console.WriteLine("Сервер запускается...");
+Console.Title = "tcp-server";
 
-int port = 5018; // Порт для подключения
-var item = "127.0.0.1"; // IP-адрес
-var listener = new TcpListener(IPAddress.Any, port); // Преобразование строки в IPAddress
+var builder = WebApplication.CreateBuilder(args);
 
+// Настройка Serilog
+Log.Logger = new LoggerConfiguration()
+	.WriteTo.Console()
+	.CreateLogger();
 
-listener.Start();
-Console.WriteLine($"Сервер запущен и слушает порт {item} {port} ");
+builder.Host.UseSerilog();
 
-while (true)
+// Проверка аргументов
+if (args.Length == 0)
 {
-	try
-	{
-		// Принимаем клиента
-		var client = await listener.AcceptTcpClientAsync();
-		Console.WriteLine($"Клиент подключен: {client.Client.RemoteEndPoint}");
-
-		using var stream = client.GetStream();
-		var buffer = new byte[256];
-
-		// Читаем сообщение от клиента
-		int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
-		var message = Encoding.UTF8.GetString(buffer, 0, bytesRead).Trim();
-		Console.WriteLine($"Получено сообщение: {message}");
-
-		// Если сообщение "ping", отправляем "pong"
-		if (message.Equals("ping", StringComparison.OrdinalIgnoreCase))
-		{
-			var responseMessage = Encoding.UTF8.GetBytes("pong");
-			await stream.WriteAsync(responseMessage, 0, responseMessage.Length);
-			Console.WriteLine("Ответ отправлен: pong");
-		}
-		else
-		{
-			Console.WriteLine("Неизвестное сообщение.");
-		}
-
-		client.Close();
-	}
-	catch (Exception ex)
-	{
-		Console.WriteLine($"Ошибка: {ex.Message}");
-	}
+	Log.Fatal("Ошибка: Параметры запуска не переданы. Ожидается формат: <host>,<port>");
+	return;
 }
+
+var parameters = args[0].Split(',');
+if (parameters.Length < 2 || !int.TryParse(parameters[1], out var port))
+{
+	Log.Fatal("Ошибка: Неверный формат параметров. Ожидается формат: <host>,<port>");
+	return;
+}
+
+var host = parameters[0];
+
+// Настройка адреса запуска
+string url = $"http://{host}:{port}";
+builder.WebHost.UseUrls(url);
+
+// Логирование адреса запуска
+Log.Information("Приложение server запускается на {Url}", url);
+
+var app = builder.Build();
+app.Run();
