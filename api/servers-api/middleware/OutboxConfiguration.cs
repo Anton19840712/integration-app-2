c7581c6b69a026016fa9373a9d9a0fdc3ce9f86d
Gridushko Anton Anatolievich <gridjushko@pit.protei.ru>
2025-02-05 00:39:19 +0300

    Вынеси работу с фоновыми процессами в отдельный статический класс на будущее.

---
+++ api/servers-api/middleware/OutboxConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Serilog;
+using servers_api.background;
+
+namespace servers_api.middleware
+{
+	public static class OutboxConfiguration
+	{
+		public static IServiceCollection AddOutboxServices(this IServiceCollection services)
+		{
+			Log.Information("Регистрация OutboxProcessor...");
+
+			// Регистрируем как IHostedService для фонового выполнения
+			services.AddHostedService<OutboxBackgroundService>();
+
+			Log.Information("OutboxProcessor зарегистрирован.");
+			return services;
+		}
+	}
+}
