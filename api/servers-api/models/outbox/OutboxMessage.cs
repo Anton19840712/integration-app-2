8dd3860e63ca07a456573f31514fa6ad887e7a25
Gridushko Anton Anatolievich <gridjushko@pit.protei.ru>
2025-02-05 00:38:51 +0300

    Добавь модель для outbox концепции.

---
+++ api/servers-api/models/outbox/OutboxMessage.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace servers_api.models.outbox
+{
+	public class OutboxMessage
+	{
+		[BsonId]
+		public ObjectId Id { get; set; }
+
+		[BsonElement("created_at")]
+		public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+		[BsonElement("processed_at")]
+		public DateTime? ProcessedAt { get; set; } // Когда сообщение обработано
+
+		[BsonElement("message")]
+		public string Message { get; set; }
+
+		[BsonElement("source")]
+		public string Source { get; set; } // IP сервера
+
+		[BsonElement("is_processed")]
+		public bool IsProcessed { get; set; } = false;
+	}
+}
