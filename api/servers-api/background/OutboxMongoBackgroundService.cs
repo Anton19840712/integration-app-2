65070ad9610ceffdf7bd8b226040f732145701db
anton gridushko - protei machine <anton.gridushko@yahoo.com>
2025-02-18 13:43:08 +0300

    Добавь метод, удаляющий сообщения из таблицы outbox созданные более, чем 24 часа назад.

--- api/servers-api/background/OutboxMongoBackgroundService.cs
+++ api/servers-api/background/OutboxMongoBackgroundService.cs
@@ -20,24 +20,24 @@ public class OutboxMongoBackgroundService : BackgroundService
 
 	protected override async Task ExecuteAsync(CancellationToken token)
 	{
-		// Этот метод будет выполняться в фоновом потоке
+		_logger.LogInformation("OutboxMongoBackgroundService запущен.");
+
+		// Параллельно запускаем фоновую очистку старых сообщений
+		_ = Task.Run(() => CleanupOldMessagesAsync(token), token);
+
 		while (!token.IsCancellationRequested)
 		{
 			try
 			{
-				// получаем сообщения из базы данных, которые мы приземлили от нашего клиента, если мы сервер:
-				// получаем сообщения, которые не были запроцешены:
 				var messages = await _outboxRepository.GetUnprocessedMessagesAsync();
+
 				foreach (var message in messages)
 				{
 					_logger.LogInformation($"Публикация сообщения: {message.Message}.");
 
-					// Отправляем сообщения в сетевую шину rabbitMQ:
-					await _rabbitMqService.PublishMessageAsync("exchange_name_tcp", "routing_key_tcp", message.Message);
-
-					// Помечаем сообщение запроцешенными: isProcessed устанавливаем в true:
-					// TODO проанализируй, если сообщений накопиться очень много, что будет.
+					await _rabbitMqService.PublishMessageAsync(message.InQueueName, message.RoutingKey, message.Message);
 					await _outboxRepository.MarkMessageAsProcessedAsync(message.Id);
+
 					_logger.LogInformation($"Обработано в Outbox: {message.Message}.");
 				}
 			}
@@ -46,8 +46,26 @@ public class OutboxMongoBackgroundService : BackgroundService
 				_logger.LogError(ex, "Ошибка при обработке Outbox.");
 			}
 
-			// Ожидаем 2 секунды перед следующим циклом проверки таблицы, которая содержит outbox сообщения
 			await Task.Delay(2000, token);
+		}
+	}
+
+	private async Task CleanupOldMessagesAsync(CancellationToken token)
+	{
+		while (!token.IsCancellationRequested)
+		{
+			try
+			{
+				int deletedCount = await _outboxRepository.DeleteOldMessagesAsync(TimeSpan.FromHours(24));
+				_logger.LogInformation($"Удалено {deletedCount} старых сообщений из Outbox.");
+			}
+			catch (Exception ex)
+			{
+				_logger.LogError(ex, "Ошибка при очистке старых сообщений Outbox.");
+			}
+
+			// Запуск каждые 10 минут
+			await Task.Delay(TimeSpan.FromMinutes(10), token);
 		}
 	}
 }
