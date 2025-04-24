b4f41595d4fd3ede860c463c40bdda610d63e3cd
anton gridushko - protei machine <anton.gridushko@yahoo.com>
2025-04-24 18:07:47 +0300

    Исправь соединение.

--- gate services/batch-processing-requests-gate/batch-processing-requests-gate/services/sftp/SftpUploader.cs
+++ gate services/batch-processing-requests-gate/batch-processing-requests-gate/services/sftp/SftpUploader.cs
@@ -6,48 +6,48 @@ using sftp_dynamic_gate_app.services.sftp;
 public class SftpUploader : ISftpUploader, IDisposable
 {
 	private readonly SftpClient _client;
+	private readonly SftpSettings _settings;
 	private readonly ILogger<SftpUploader> _logger;
-	private readonly string _remotePath;
-	private readonly IConfiguration _configuration;
 
-	public SftpUploader(IOptions<SftpSettings> options, ILogger<SftpUploader> logger, IConfiguration configuration)
+	public SftpUploader(IOptions<SftpSettings> options, ILogger<SftpUploader> logger)
 	{
-		var settings = options.Value;
+		_settings = options.Value;
+
 		_logger = logger;
-		_remotePath = settings.Source ?? ".";
-		_configuration = configuration;
-		_client = new SftpClient(settings.Host, settings.Port, settings.UserName, settings.Password);
+
+		_client = new SftpClient(
+			_settings.Host,
+			_settings.Port,
+			_settings.UserName,
+			_settings.Password);
+
 		_client.Connect();
 	}
 
 	public async Task UploadAsync(string localFilePath, string remoteFileName = null)
 	{
-		var settings = new SftpSettings
-		{
-			Host = "172.16.205.118",
-			Port = 22,
-			UserName = "tester",
-			Password = "password",
-			Source = "." // Папка на SFTP
-		};
-
 		try
 		{
-			using var client = new SftpClient(settings.Host, settings.Port, settings.UserName, settings.Password);
+			using var client = new SftpClient(
+				_settings.Host,
+				_settings.Port,
+				_settings.UserName,
+				_settings.Password);
+
 			client.Connect();
 
 			if (!client.IsConnected)
 				throw new Exception("Не удалось подключиться к SFTP");
 
 			string fileName = remoteFileName ?? Path.GetFileName(localFilePath);
-			string remoteFullPath = settings.Source.EndsWith("/")
-				? settings.Source + fileName
-				: settings.Source + "/" + fileName;
+			string remoteFullPath = _settings.Source.EndsWith("/")
+				? _settings.Source + fileName
+				: _settings.Source + "/" + fileName;
 
 			await using var fileStream = File.OpenRead(localFilePath);
 			client.UploadFile(fileStream, remoteFullPath);
 
-			_logger.LogInformation("Файл '{FileName}' успешно загружен в SFTP в папку '{RemotePath}'", fileName, settings.Source);
+			_logger.LogInformation("Файл '{FileName}' успешно загружен в SFTP в папку '{RemotePath}'", fileName, _settings.Source);
 		}
 		catch (Exception ex)
 		{
@@ -55,8 +55,6 @@ public class SftpUploader : ISftpUploader, IDisposable
 			throw;
 		}
 	}
-
-
 
 	public void Dispose()
 	{
